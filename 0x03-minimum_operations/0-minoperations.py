4defd9e9801c1ea95ca3fe34c0e6655edc35c020
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-05-13 14:17:07 -0500

    Corrected typo

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -13,7 +13,7 @@ def minOperations(n):
 
     for i in range(2, int(sqrt(n) + 1)):
         while n % i == 0:
-            min_ops += + i
+            min_ops += i
             n = n / i
             if n <= 1:
                 break
