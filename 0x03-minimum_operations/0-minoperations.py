1f357092f466673f42eb717ef5c42cbf51ff482f
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-05-13 14:15:59 -0500

    Completed project

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Finds the minimun operations needed to have a given amount of H characters
+"""
+
+from math import sqrt
+
+
+def minOperations(n):
+    min_ops = 0
+
+    if n <= 1:
+        return 0
+
+    for i in range(2, int(sqrt(n) + 1)):
+        while n % i == 0:
+            min_ops += + i
+            n = n / i
+            if n <= 1:
+                break
+
+    min_ops += int(n)
+
+    return min_ops
